460de2e3156fa76d62469116bd3d046fadb506bc
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-12-24 09:52:21 +0530

    tmp

--- upload.sh
+++ upload.sh
@@ -1,21 +1,4 @@
-#!/bin/bash
+#/bin/#!/usr/bin/env bash
 
-# Exit if No File is Specified
-if [[ "$#"  ==  '0' ]]; then
-echo  -e 'ERROR: No File Specified!' && exit 1
-fi
-
-# File to Upload
-FILE="@$1"
-
-# Find the Best server to upload
-SERVER=$(curl -s https://apiv2.gofile.io/getServer | jq  -r '.data|.server')
-
-UPLOAD=$(curl -F file=${FILE} https://${SERVER}.gofile.io/uploadFile)
-
-LINK=$(echo $UPLOAD | jq -r '.data|.downloadPage')
-
-# Print the link!
-echo $LINK
-
-echo " "
+bash "$OUT_FILE" upload.sh
+gh release create "v-$BUILD_TIME" "$OUT_FILE" --generate-notes
